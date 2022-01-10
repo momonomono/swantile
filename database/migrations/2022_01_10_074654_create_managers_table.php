4b12701a253062d96877cc8a6e0c7d935b2189a7
momonomono <momonomono1128@gmail.com>
2022-01-10 16:52:06 +0900

    migrationを書き換えました

--- database/migrations/2022_01_10_074654_create_managers_table.php
+++ database/migrations/2022_01_10_074654_create_managers_table.php
@@ -15,6 +15,7 @@ class CreateManagersTable extends Migration
     {
         Schema::create('managers', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
