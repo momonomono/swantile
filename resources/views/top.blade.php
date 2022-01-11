bb44ab2cfbcd5d90945b47e1cd05f3d7990af42a
momonomono <momonomono1128@gmail.com>
2022-01-11 09:52:34 +0900

    データベースが完成しました

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.layout')
+
+@section('title','トップページ')
+
+@section('body')
     <div class="l-container" id="app">
        <top-component></top-component>
     </div>
-</body>
-<script src="{{ asset('js/app.js') }}"></script>
-</html>
+@endsection
